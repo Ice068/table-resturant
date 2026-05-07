152062073a831d33c6aed3f29c2441b6594ea084
Tanatap Treerapragon <thanathep.ice@gmail.com>
2026-05-07 22:15:19 +0700

    Add static file serving for frontend
    Serve static files from the frontend directory and set up a route to send the index.html file.

--- backend/server.js
+++ backend/server.js
@@ -4,11 +4,21 @@ const jwt = require("jsonwebtoken");
 const bcrypt = require("bcrypt");
 const cors = require("cors");
 
+const path = require('path');
+const express = require('express');
+
+
 const app = express();
 app.use(cors());
 app.use(express.json());
 
 const SECRET = "mysecretkey";
+
+app.use(express.static(path.join(__dirname, '../frontend')));
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../frontend/index.html'));
+});
+
 
 // ==========================
 // INIT ADMIN
