f0a4c0b8504027a80cfaa0ed7724ec9489e62194
Tanatap Treerapragon <thanathep.ice@gmail.com>
2026-05-07 16:52:24 +0000

    fix

--- frontend/login.js
+++ frontend/login.js
@@ -13,7 +13,7 @@ document.getElementById('loginForm').addEventListener('submit', async function(e
 
     try {
 
-        const res = await fetch("hhttps://resturant-duo.onrender.com/login", {
+        const res = await fetch("https://resturant-duo.onrender.com/login", {
             method: "POST",
             headers: {
                 "Content-Type": "application/json"
